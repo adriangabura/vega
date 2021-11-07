fc9b36984a47e686819b43548534738e6084540b
Adrian Gabura <generic@adriangabura.name>
2021-11-07 18:22:05 +0200

    [LIB-5] test_get_bucket

--- librarius/tests/boto3_mapper/test_operations.py
+++ librarius/tests/boto3_mapper/test_operations.py
@@ -2,6 +2,11 @@ import pytest
 from librarius.adapters import boto_mapper
 
 
+def test_get_bucket(default_test_bucket, boto3_default_resource, boto3_default_client):
+    bucket = boto_mapper.BucketOperation.get(boto3_default_resource, boto3_default_client, default_test_bucket.name)
+    assert bucket.name == default_test_bucket.name
+
+
 def test_delete_bucket(default_test_bucket, boto3_default_resource, boto3_default_client):
     default_test_bucket.delete()
     retrieve_result = boto_mapper.BucketOperation.get(
