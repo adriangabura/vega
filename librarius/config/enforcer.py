5bd7b6b117deda1492c488e09a145ac74442693b
Adrian Gabura <mail@adriangabura.name>
2021-11-21 18:20:34 +0200

    Experiments

--- librarius/config/enforcer.py
+++ librarius/config/enforcer.py
@@ -1,11 +1,16 @@
 import casbin
+from casbin.model import Model
+from casbin import util
 import pathlib
 
-__CASBIN_MODEL__ = pathlib.Path(__file__).parent / "rbac_model.conf"
-__CASBIN_POLICY__ = pathlib.Path(__file__).parent / "rbac_policy.csv"
+__CASBIN_MODEL__ = pathlib.Path(__file__).parent / "test.conf"
+__CASBIN_POLICY__ = pathlib.Path(__file__).parent / "test.csv"
 
 e = casbin.Enforcer(__CASBIN_MODEL__.__str__(), __CASBIN_POLICY__.__str__())
+e.add_named_domain_matching_func("g", util.key_match2)
 
+sm = e.enforce("root", "/login", "GET", "domain1")
+print(sm)
 #e.delete_role("policies_admin")
 #e.remove_policy("policies_admin", "/policies/*", "*")
 #e.add_role_for_user("root", "policies_admin")
