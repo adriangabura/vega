84ea85ce3956a7232c934428bcd40be88d2dbf5c
Adrian Gabura <mail@adriangabura.name>
2021-11-21 08:27:57 +0200

    Update Pipfile

--- librarius/entrypoints/routers/policies.py
+++ librarius/entrypoints/routers/policies.py
@@ -6,6 +6,13 @@ from fastapi import APIRouter, Depends, HTTPException, status, Response, Form
 
 router = APIRouter(tags=["policy"])
 
+from pydantic import BaseModel
+
+
+class PolicyPutBody(BaseModel):
+    resource: str
+    action: str
+
 
 @router.get("/policy/")
 def get_policy():
@@ -13,8 +20,8 @@ def get_policy():
 
 
 @router.put("/policy/{policy_name}")
-def put_policy(policy_name: str):
-    return True
+def put_policy(policy_name: str, policy_body: PolicyPutBody):
+    return policy_body
 
 
 @router.delete("/policy/{policy_name}")
