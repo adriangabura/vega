95cef92fa99942d8fdc42b123a4a81c27429a19c
Adrian Gabura <mail@adriangabura.name>
2021-11-22 23:02:05 +0200

    Fix enforcer test

--- librarius/config/enforcer.py
+++ librarius/config/enforcer.py
@@ -7,10 +7,10 @@ __CASBIN_MODEL__ = pathlib.Path(__file__).parent / "test.conf"
 __CASBIN_POLICY__ = pathlib.Path(__file__).parent / "test.csv"
 
 e = casbin.Enforcer(__CASBIN_MODEL__.__str__(), __CASBIN_POLICY__.__str__())
-e.add_named_domain_matching_func("g", util.key_match2)
+e.add_named_domain_matching_func("g", util.key_match2_func)
 
-sm = e.enforce("user2", "/users/54", "GET", "domainh")
-print(sm)
+result = e.enforce("user2", "/users/", "GET", "domain")
+print(result)
 #e.delete_role("policies_admin")
 #e.remove_policy("policies_admin", "/policies/*", "*")
 #e.add_role_for_user("root", "policies_admin")
