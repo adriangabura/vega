e822978c9e9d5f3e2a978a67e79dc788f9f61b69
Adrian Gabura <mail@adriangabura.name>
2021-11-25 15:18:08 +0200

    Modify tests

--- tests/integration/test_casbin.py
+++ tests/integration/test_casbin.py
@@ -10,4 +10,12 @@ pytestmark = pytest.mark.usefixtures("casbin_policy_blank")
 @pytest.mark.usefixtures("supergroup_role")
 def test_supergroup_role(casbin_enforcer):
     ce = casbin_enforcer
-    #assert ce.enforce("superadmin", "/users/2121", "GET", "domain")
+    assert ce.enforce("superadmin", "/wrong_endpoint/", "GET", "domain") == False
+    assert ce.enforce("superadmin", "/users/", "GET", "domain")
+    assert ce.enforce("superadmin", "/users/1989", "GET", "domain")
+    assert ce.enforce("superadmin", "/groups/", "GET", "domain")
+    assert ce.enforce("superadmin", "/groups/1989", "GET", "domain")
+    assert ce.enforce("superadmin", "/policies/", "GET", "domain")
+    assert ce.enforce("superadmin", "/policies/1989", "GET", "domain")
+    assert ce.enforce("superadmin", "/roles/", "GET", "domain")
+    assert ce.enforce("superadmin", "/roles/1989", "GET", "domain")
