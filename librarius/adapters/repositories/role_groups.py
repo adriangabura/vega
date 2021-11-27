fbd881217d5befe7dfed9d81076204782e8f7280
Adrian Gabura <mail@adriangabura.name>
2021-11-27 21:18:59 +0200

    Add RoleGroup Repository

---
+++ librarius/adapters/repositories/role_groups.py
@@ -0,0 +1,29 @@
+import typing as tp
+from librarius.domain.models import RoleGroup
+from librarius.adapters.repositories.abstract import AbstractRepository
+from librarius.adapters.repositories.contexts import SQLAlchemyRepositoryContext
+
+
+class RoleGroupsRepository(
+    AbstractRepository["RoleGroupsRepository", SQLAlchemyRepositoryContext]
+):
+    name: tp.ClassVar[str] = "roles"
+
+    def add(self, role_group: "RoleGroup") -> None:
+        self.context.add(role_group)
+        # AuthorCreated Event
+        self.touched.add(role_group)
+
+    def remove(self, role_group: "RoleGroup") -> None:
+        self.context.remove(role_group)
+        self.touched.add(role_group)
+
+    def find(self):
+        pass
+        # return self.context.query(query_object)
+
+    def find_by_uuid(self, uuid) -> tp.Optional["RoleGroup"]:
+        return self.context.session.query(RoleGroup).filter_by(uuid=uuid).first()
+
+    def find_by_name(self, name: str) -> tp.Optional["RoleGroup"]:
+        return self.context.session.query(RoleGroup).filter_by(name=name).first()
